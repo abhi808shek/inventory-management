e03ba2fde9245e47f58ef15317488fb46040e605
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2024-12-03 14:08:55 +0530

    added setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import './App.module.css';
+import handleAsync from './utils/handleAsync';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onLoad = handleAsync(async() => {
+    setLoading(true);
+  }, () => setLoading(false));
+
+  useEffect(() => {
+    onLoad();
+  }, []);
 
   return (
     <h1>Hello World</h1>
   )
 }
 
-export default App
+export default App;
