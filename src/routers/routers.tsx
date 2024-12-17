82e3f743ce27e6b63f6caba3fb7481ebbafc127b
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2024-12-18 02:18:23 +0530

    Login UI Created

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -1,14 +1,20 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-//pages
-const Home = lazy(() => import("../pages/home"));
+// Layouts
+const AuthLayout = lazy(() => import("@/layout/AuthLayout"));
 
+//Pages
+const LoginPage = lazy(() => import("@/pages/login"));
+const SignupPage = lazy(() => import("@/pages/signup"));
 const Routers = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
       </Routes>
     </Suspense>
   );
