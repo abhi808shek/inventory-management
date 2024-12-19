4e1b6e3c694e1074e1b8e019493a96200228a630
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2024-12-20 02:05:19 +0530

    Signup Api Integrated

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,15 @@
 import AXIOS from "@/configs/axios.config";
 
+// Login Api Async Function
 export const loginApi = async (loginCredentential: any) => {
   const endpoint = "/login";
   const result = await AXIOS.post(endpoint, loginCredentential);
+  return result;
+};
+
+// Singup Api Async Function
+export const signupApi = async (signupCredentential: any) => {
+  const endpoint = "/registration";
+  const result = await AXIOS.post(endpoint, signupCredentential);
   return result;
 };
