2e9e11d7ad4e21394828ea57cc945d608013f534
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2024-12-20 01:55:57 +0530

    Login Api Integrated and RTK Tanstack Query Impleted

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
+  </QueryClientProvider>
 );
