aeeb6ac74f0f16f2bf9b3c7efed444d5fdd8e594
Sourav Roy <sroy3166@gmail.com>
2024-12-30 00:30:07 +0530

    fixed multi api call

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,37 @@ import Routers from "./routers/routers";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getUserData } from "@/api/user.api";
+import LoadingComponent from "@/components/Loading";
 import "./App.css";
 import { setUserData } from "./store/user/user-reducer";
 import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const App = () => {
-  const userDeatils = useQuery({ queryKey: ["user"], queryFn: getUserData });
+  const { data, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: getUserData,
+    enabled: false,
+    staleTime: Infinity,
+  });
 
   const dispatch = useDispatch();
 
-  dispatch(setUserData(userDeatils.data?.data ?? null));
+  dispatch(setUserData(data?.data ?? null));
 
   return (
     <ErrorBoundary>
       <Toaster />
       <BrowserRouter>
-        <Routers />
+        {isLoading ? (
+          <div className="w-full h-[100svh] flex items-center justify-center">
+            <LoadingComponent />
+          </div>
+        ) : (
+          <Routers />
+        )}
       </BrowserRouter>
     </ErrorBoundary>
   );
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+import LOADING_IMAGE from "@/assets/images/d9f21515b1e38d83e94fdbce88f623b6.gif";
+
+const LoadingComponent = () => {
+  return <img src={LOADING_IMAGE} width={200} height={200} />;
+};
+
+export default LoadingComponent;
