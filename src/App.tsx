7c40695f048b05606e2244ce09cf1329f6eec322
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2025-01-08 00:33:56 +0530

    Sidebar Permissions and Users APi commented

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,42 @@ import Routers from "./routers/routers";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { getUserData } from "@/api/user.api";
-import LoadingComponent from "@/components/Loading";
-import { setUserData } from "./store/user/user-reducer";
-import { useDispatch } from "react-redux";
-import useApi from "@/hooks/useApi";
-import handleAsync from "@/utils/handleAsync";
 import "./App.css";
-import { useEffect } from "react";
+// import { getUserData } from "@/api/user.api";
+// import LoadingComponent from "@/components/Loading";
+// import { setUserData } from "./store/user/user-reducer";
+// import { useDispatch } from "react-redux";
+// import useApi from "@/hooks/useApi";
+// import handleAsync from "@/utils/handleAsync";
+// import { useEffect } from "react";
 // import { customLocalStorage } from "./utils/customLocalStorage";
 
 const App = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const fetchUser = handleAsync(async () => {
-    const res = await getUserData();
-    dispatch(setUserData(res.data?.data ?? null));
-    return res;
-  });
+  // const fetchUser = handleAsync(async () => {
+  //   const res = await getUserData();
+  //   dispatch(setUserData(res.data?.data ?? null));
+  //   return res;
+  // });
 
-  const { pending, execute } = useApi(fetchUser);
-  // const token = customLocalStorage.getData("token");
-  useEffect(() => {
-    execute();
-  }, []);
+  // const { pending, execute } = useApi(fetchUser);
+  // // const token = customLocalStorage.getData("token");
+  // useEffect(() => {
+  //   execute();
+  // }, []);
 
   return (
     <ErrorBoundary>
       <Toaster />
       <BrowserRouter>
-        {pending ? (
-          <div className="w-full h-[100svh] flex items-center justify-center">
+        {/* {pending ? ( */}
+        {/* <div className="w-full h-[100svh] flex items-center justify-center">
             <LoadingComponent />
-          </div>
-        ) : (
-          <Routers />
-        )}
+          </div> */}
+        {/* ) : (
+        )} */}
+        <Routers />
       </BrowserRouter>
     </ErrorBoundary>
   );
