0dba6ea8cdb314ab2d5ed8d312c95134ae76f4c8
Sourav Roy <sroy3166@gmail.com>
2025-01-11 19:17:24 +0530

    changed sidebar color

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // Ensures build output is in a folder named `dist`
+  },
+  server: {
+    port: 5173, // Local dev server port
+    open: true, // Opens the app in the browser on start
+    host: true, // Allows external devices to access the app
+  },
+  preview: {
+    port: 5000, // Preview server port
+    host: true, // External access during preview
+    strictPort: true, // Ensures the port is fixed
+  },
 });
