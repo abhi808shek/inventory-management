c0e24bc23c026fa032422b35662e119fdf2f5afc
Abhishek Gupta <abhishek@Abhishek-ka-Mac-mini.local>
2025-01-11 14:38:20 +0530

    Dynamic Routes made for Update and Add Form

--- src/assets/data/routeOptions.ts
+++ src/assets/data/routeOptions.ts
@@ -1,5 +1,4 @@
-import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
 
 // Users routes Page
 const Users = lazy(() => import("@/pages/users/index.tsx"));
